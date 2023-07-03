0bbefce32d174001c380d587f6119e322be40f5e
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-03 19:47:25 +0900

    ⚡️ :: UserAddRequest 수정

--- OnePiece/src/main/java/com/example/onepiece/domain/controller/dto/request/UserAddRequest.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/controller/dto/request/UserAddRequest.java
@@ -1,4 +1,19 @@
 package com.example.onepiece.domain.controller.dto.request;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class UserAddRequest {
+
+    private String userId;
+
+    private String userPassword;
+
+    private String nickname;
+
+    private Integer classNumber;
+
+    private Integer studentNumber;
 }
