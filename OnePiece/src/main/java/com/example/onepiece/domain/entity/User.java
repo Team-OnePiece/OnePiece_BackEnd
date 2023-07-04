bcd0a0e0a90f787dd0bd35cdfbfbe30e16087354
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-04 19:17:25 +0900

    ⚡️ :: Id정규식 수정

--- OnePiece/src/main/java/com/example/onepiece/domain/entity/User.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/entity/User.java
@@ -22,11 +22,11 @@ public class User {
     private String userId;
 
     @NotBlank(message = "비밀번호는 필수 입력 값 입니다")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{1,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{1,16}", message = "비밀번호는 1~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String userPassword;
 
     @NotBlank
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{1,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,9}$", message = "닉네임은 특수문자를 제외한 2~9자리여야 합니다.")
     private String nickname;
 
     @Column(name = "profile_image")
