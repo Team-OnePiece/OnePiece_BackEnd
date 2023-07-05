c9a6a28d2279431ea1cd74504f846481abf63a85
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-06 01:30:45 +0900

    ⚡️ :: ErrorResponseEntity 추가

--- OnePiece/src/main/java/com/example/onepiece/global/security/error/entity/ErrorResponseEntity.java
+++ OnePiece/src/main/java/com/example/onepiece/global/security/error/entity/ErrorResponseEntity.java
@@ -1,9 +1,26 @@
 package com.example.onepiece.global.security.error.entity;
 
+import com.example.onepiece.global.security.error.ErrorCode;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.ResponseEntity;
 
 @Data
 @Builder
 public class ErrorResponseEntity {
+
+    private int status;
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ErrorResponseEntity> responseEntity(ErrorCode errorCode){
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponseEntity.builder()
+                        .status(errorCode.getHttpStatus())
+                        .code(errorCode.name())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
+    }
 }
