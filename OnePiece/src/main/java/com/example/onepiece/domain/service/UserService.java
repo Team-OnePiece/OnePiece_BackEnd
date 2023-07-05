9bdfea6ce063f38dd8e3aab3491cc846333ad30a
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 13:37:27 +0900

    ⚰️ :: signup기능 삭제

--- OnePiece/src/main/java/com/example/onepiece/domain/service/UserService.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/service/UserService.java
@@ -1,37 +1,21 @@
 package com.example.onepiece.domain.service;
 
 import com.example.onepiece.domain.controller.dto.request.UserAddRequest;
-import com.example.onepiece.domain.entity.User;
 import com.example.onepiece.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final Duplicate duplicate;
     private final UserRepository userRepository;
 
     @Transactional
-    public void addUser(UserAddRequest request){
-        String userId = request.getUserId();
-        Optional<User> user = userRepository.findByUserId(userId);
-        if(user.isEmpty()){
-            userRepository.save(
-                    User.builder()
-                            .userId(request.getUserId())
-                            .userPassword(request.getUserPassword())
-                            .nickname(request.getNickname())
-                            .profile(request.getProfile())
-                            .classNumber(request.getClassNumber())
-                            .studentNumber(request.getStudentNumber())
-                            .build());
-        }
-        else {
-            throw new IllegalArgumentException("중복된 아이디");
-        }
+    public void signUpUser(UserAddRequest request) {
+
     }
 }
