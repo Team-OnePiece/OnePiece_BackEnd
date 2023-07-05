b56ce9cc414fff0f4bc2761f3db64b59d5ce8302
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 15:16:22 +0900

     ⚡️:: entity정규식 삭제

--- OnePiece/src/main/java/com/example/onepiece/domain/entity/User.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/entity/User.java
@@ -18,27 +18,16 @@ public class User {
     private Long id;
 
 
-    @NotBlank(message = "아이디는 필수 입력 값 입니다")
-    @Pattern(regexp = "(?=.[0-9])(?=.*[a-zA-Z]).{1,20}", message = "아이디는 1~ 20자 영문 대 소문자, 숫자를 사용하세요")
     private String userId;
 
-    @NotBlank(message = "비밀번호는 필수 입력 값 입니다")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{1,16}", message = "비밀번호는 1~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String userPassword;
 
-    @NotBlank
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,9}$", message = "닉네임은 특수문자를 제외한 2~9자리여야 합니다.")
     private String nickname;
 
-    @Column(name = "profile_image")
     private String profile;
 
-    @NotNull
-    @Min(1) @Max(3)
     private Integer classNumber;
 
-    @NotNull
-    @Min(1) @Max(16)
     private Integer studentNumber;
 
     @Builder
