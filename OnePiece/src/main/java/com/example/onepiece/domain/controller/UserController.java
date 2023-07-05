0284c6ebced869f94b379d88af2174d33c420eb2
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 15:14:47 +0900

    ⚡️ :: userController에 id중복확인 기능 추가

--- OnePiece/src/main/java/com/example/onepiece/domain/controller/UserController.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.onepiece.domain.controller;
 
 import com.example.onepiece.domain.controller.dto.request.UserAddRequest;
+import com.example.onepiece.domain.controller.dto.request.UserDuplicateNicknameRequest;
+import com.example.onepiece.domain.controller.dto.request.UserDuplicateUserIdRequest;
+import com.example.onepiece.domain.service.Duplicate;
 import com.example.onepiece.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -16,10 +16,17 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
+    private final Duplicate duplicate;
 
     @PostMapping
-    public void userAdd(@RequestBody @Valid UserAddRequest request){
+    public void userSignUp(@RequestBody @Valid UserAddRequest request){
         userService.signUpUser(request);
     }
 
+    @GetMapping("/idDuplicate")
+    public Boolean userIdDuplicate(@RequestBody @Valid UserDuplicateUserIdRequest request){
+       return duplicate.idDuplicate(request);
+    }
+
+  
 }
