fa0f8154b71d150bb4ba5f4e95e42313e6c7d079
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 13:38:07 +0900

    ♻️ :: 메소드 네이밍 수정

--- OnePiece/src/main/java/com/example/onepiece/domain/controller/UserController.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.onepiece.domain.controller;
 import com.example.onepiece.domain.controller.dto.request.UserAddRequest;
 import com.example.onepiece.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +19,7 @@ public class UserController {
 
     @PostMapping
     public void userAdd(@RequestBody @Valid UserAddRequest request){
-        userService.addUser(request);
+        userService.signUpUser(request);
     }
 
 }
