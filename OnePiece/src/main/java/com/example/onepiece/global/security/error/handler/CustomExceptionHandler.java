d375c13c8f6a3f55e7df03d18fac5a9cb94afb73
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-06 01:36:50 +0900

    ⚡️:: CustomExceptionHandler추가

--- OnePiece/src/main/java/com/example/onepiece/global/security/error/handler/CustomExceptionHandler.java
+++ OnePiece/src/main/java/com/example/onepiece/global/security/error/handler/CustomExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.example.onepiece.global.security.error.handler;
 
+import com.example.onepiece.global.security.error.entity.ErrorResponseEntity;
+import com.example.onepiece.global.security.error.exception.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException customException){
+        return ErrorResponseEntity.responseEntity(customException.getErrorCode());
+    }
 }
