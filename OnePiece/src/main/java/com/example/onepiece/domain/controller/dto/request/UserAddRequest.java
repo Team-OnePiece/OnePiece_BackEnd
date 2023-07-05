c46a0203b4c40b4e8a761afe23eb7f92375ccb2a
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-05 15:13:28 +0900

    ⚡️ :: userRequest에 정규식 추가

--- OnePiece/src/main/java/com/example/onepiece/domain/controller/dto/request/UserAddRequest.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/controller/dto/request/UserAddRequest.java
@@ -3,19 +3,33 @@ package com.example.onepiece.domain.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.validation.constraints.*;
+
 @NoArgsConstructor
 @Getter
 public class UserAddRequest {
 
+    @NotBlank
     private String userId;
 
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{1,16}", message = "비밀번호는 1~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String userPassword;
 
+    @NotBlank
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,9}$", message = "닉네임은 특수문자를 제외한 2~9자리여야 합니다.")
     private String nickname;
 
+    @Column(name = "profile_image")
     private String profile;
 
+    @NotNull
+    @Min(1) @Max(3)
     private Integer classNumber;
 
+    @NotNull
+    @Min(1) @Max(16)
     private Integer studentNumber;
+
 }
