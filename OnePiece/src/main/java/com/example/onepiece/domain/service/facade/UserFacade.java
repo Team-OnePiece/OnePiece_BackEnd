61d6e146360424cef9403842d1f14622687f06b7
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-06 01:40:59 +0900

    ⚡️:: UserFacade에 CustomExeption추가

--- OnePiece/src/main/java/com/example/onepiece/domain/service/facade/UserFacade.java
+++ OnePiece/src/main/java/com/example/onepiece/domain/service/facade/UserFacade.java
@@ -2,11 +2,11 @@ package com.example.onepiece.domain.service.facade;
 
 import com.example.onepiece.domain.entity.User;
 import com.example.onepiece.domain.repository.UserRepository;
+import com.example.onepiece.global.security.error.ErrorCode;
+import com.example.onepiece.global.security.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.Column;
 
 @Component
 @RequiredArgsConstructor
@@ -17,19 +17,19 @@ public class UserFacade {
     public User currentUser(){
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("계정이 없습니다"));
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     public boolean existsByUserId(String userId){
     if(userRepository.existsByUserId(userId)){
-            throw new  IllegalArgumentException("이미 사용하는 아이디 입니다");
+            throw new CustomException(ErrorCode.USER_ALREADY_EXISTS);
         }
     else return true;
     }
 
     public boolean existsByNickname(String nickname){
         if (userRepository.existsByNickname(nickname)){
-            throw new IllegalArgumentException("이미 사용하고 있는 별명 입니다");
+            throw new CustomException(ErrorCode.NICKNAME_ALREADY_EXISTS);
         }
         else return true;
     }
