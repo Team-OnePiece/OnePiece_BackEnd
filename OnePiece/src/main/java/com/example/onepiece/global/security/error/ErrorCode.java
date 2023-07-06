0974ae5b7cefcee63dc92c636349b4501ffd0e95
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-06 11:27:38 +0900

    ⚡️ :: ErrorCodedp 500추가

--- OnePiece/src/main/java/com/example/onepiece/global/security/error/ErrorCode.java
+++ OnePiece/src/main/java/com/example/onepiece/global/security/error/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(404, "User Not Found"),
     USER_ALREADY_EXISTS(409, "User Already Exists"),
-    NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists");
+    NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
+
+    INTERNAL_SERVER_ERROR(500, "");
 
     private final Integer httpStatus;
     private final String message;
