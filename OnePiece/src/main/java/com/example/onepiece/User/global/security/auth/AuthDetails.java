8d35478fe741df096a753c88d2cd993881cf613d
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-07 18:33:01 +0900

    ⚡️::  AuthDetails 추가

--- OnePiece/src/main/java/com/example/onepiece/User/global/security/auth/AuthDetails.java
+++ OnePiece/src/main/java/com/example/onepiece/User/global/security/auth/AuthDetails.java
@@ -12,4 +12,42 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class AuthDetails implements UserDetails {
 
+    private final User user;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
