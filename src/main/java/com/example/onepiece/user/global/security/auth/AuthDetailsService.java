02ba652d580da26ced0e712e60311f23ac06044a
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-09 02:10:24 +0900

    ♻️ :: 공백

--- src/main/java/com/example/onepiece/user/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/example/onepiece/user/global/security/auth/AuthDetailsService.java
@@ -7,6 +7,7 @@ import com.example.onepiece.user.global.security.error.exception.CustomException
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,9 +17,9 @@ public class AuthDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String userId){
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException{
         User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new UsernameNotFoundException("아이디가 존재하지 않습니다"));
         return new AuthDetails(user);
     }
 }
