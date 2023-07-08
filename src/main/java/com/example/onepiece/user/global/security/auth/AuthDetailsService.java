85e2848eb7de099f9873faacd7a1110523b7b559
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-09 01:10:52 +0900

    ⚡️:: AuthDetailsService

--- src/main/java/com/example/onepiece/user/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/example/onepiece/user/global/security/auth/AuthDetailsService.java
@@ -1,4 +1,24 @@
 package com.example.onepiece.user.global.security.auth;
 
-public class AuthDetailsService {
+import com.example.onepiece.user.domain.entity.User;
+import com.example.onepiece.user.domain.repository.UserRepository;
+import com.example.onepiece.user.global.security.error.ErrorCode;
+import com.example.onepiece.user.global.security.error.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId){
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        return new AuthDetails(user);
+    }
 }
