b5c30a673f23956ec4e04a885517372e48f491e4
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-09 01:41:17 +0900

    🐛 :: controller버그수정

--- src/main/java/com/example/onepiece/user/domain/controller/UserController.java
+++ src/main/java/com/example/onepiece/user/domain/controller/UserController.java
@@ -8,10 +8,9 @@ import com.example.onepiece.user.domain.service.UserSignUpService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Tag(name = "UserAPI입니다")
 @RestController
@@ -24,19 +23,19 @@ public class UserController {
 
     @Operation(summary = "회원가입")
     @PostMapping("/signup")
-    public void signUp(UserSignUpRequest request){
+    public void signUp(@RequestBody @Valid UserSignUpRequest request){
         userSignUpService.userSignUp(request);
     }
 
     @Operation(summary = "아이디 중복확인")
     @GetMapping("/idDuplicate")
-    public void userIdDuplicate(IdDuplicateRequest request){
+    public void userIdDuplicate(@RequestBody IdDuplicateRequest request){
         duplicateService.checkUserIdDuplicate(request);
     }
 
     @Operation(summary = "별명 중복확인")
     @GetMapping("/nicknameDuplicate")
-    public void nicknameDuplicate(NicknameDuplicateRequest request){
+    public void nicknameDuplicate(@RequestBody NicknameDuplicateRequest request){
         duplicateService.checkNicknameDuplicate(request);
     }
 
