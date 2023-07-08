a8f25783ecc8e44be2f89f965d1043dd973f7e33
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-09 01:02:57 +0900

    📑 :: 패키지 추가

---
+++ src/main/java/com/example/onepiece/user/domain/controller/dto/request/IdDuplicateRequest.java
@@ -0,0 +1,10 @@
+package com.example.onepiece.user.domain.controller.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class IdDuplicateRequest {
+    private String userId;
+}
---
+++ src/main/java/com/example/onepiece/user/global/security/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.onepiece.user.global.security.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND(404, "User Not Found"),
+    USERId_ALREADY_EXISTS(409, "UserId Already Exists"),
+    NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
+
+    INTERNAL_SERVER_ERROR(500, "");
+
+    private final Integer httpStatus;
+    private final String message;
+}
