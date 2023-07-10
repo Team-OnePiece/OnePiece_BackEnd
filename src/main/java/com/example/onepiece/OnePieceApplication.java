0a24f326a2502dc752a8fbb7893c77ce601d34d1
Yoohaeun <haspring@dsm.hs.kr>
2023-07-10 17:03:13 +0900

    test commit

--- src/main/java/com/example/onepiece/OnePieceApplication.java
+++ src/main/java/com/example/onepiece/OnePieceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnePieceApplication {
-
+    //tesasdf
     public static void main(String[] args) {
         SpringApplication.run(OnePieceApplication.class, args);
     }
