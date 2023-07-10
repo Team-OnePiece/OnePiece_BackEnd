fca0f5384b2f520c6d23cb9cb268d9a86914c767
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-10 22:25:00 +0900

    📑 :: loginService추가

---
+++ src/main/java/com/example/onepiece/user/domain/service/UserLoginService.java
@@ -0,0 +1,17 @@
+package com.example.onepiece.user.domain.service;
+
+import com.example.onepiece.user.domain.controller.dto.request.UserLoginRequest;
+import com.example.onepiece.user.domain.entity.User;
+import com.example.onepiece.user.domain.service.facade.UserFacade;
+import com.example.onepiece.user.global.security.Jwt.dto.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserLoginService {
+
+
+
+}
