0ee721c844470e67e3a886c98e4f482144a1a782
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-11 10:48:59 +0900

    ⚡️:: 예외처리 수정

--- src/main/java/com/example/onepiece/user/domain/service/UserLoginService.java
+++ src/main/java/com/example/onepiece/user/domain/service/UserLoginService.java
@@ -5,6 +5,7 @@ import com.example.onepiece.user.domain.entity.User;
 import com.example.onepiece.user.domain.service.facade.UserFacade;
 import com.example.onepiece.user.global.error.ErrorCode;
 import com.example.onepiece.user.global.error.exception.CustomException;
+import com.example.onepiece.user.global.error.exception.PasswordMIssMatchException;
 import com.example.onepiece.user.global.security.Jwt.JwtProvider;
 import com.example.onepiece.user.global.security.Jwt.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +25,7 @@ public class UserLoginService {
         User user = userFacade.getUserByUserId(request.getUserId());
 
         if (!passwordEncoder.matches(request.getUserPassword(), user.getUserPassword())) {
-            throw new CustomException(ErrorCode.PASSWORD_MISS_MATCH);
+            throw PasswordMIssMatchException.EXCEPTION;
         }
         return jwtProvider.getToken(user);
     }
