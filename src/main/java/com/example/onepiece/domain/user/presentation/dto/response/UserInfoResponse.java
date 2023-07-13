52f20cce5a62ee80c1bed9a8b76c1964a0f928d5
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-13 22:27:44 +0900

    📑 :: 패키지 추가

---
+++ src/main/java/com/example/onepiece/domain/user/presentation/dto/response/UserInfoResponse.java
@@ -0,0 +1,9 @@
+package com.example.onepiece.domain.user.presentation.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserInfoResponse {
+}
---
+++ src/main/java/com/example/onepiece/domain/user/service/UserInfoService.java
@@ -0,0 +1,9 @@
+package com.example.onepiece.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserInfoService {
+}
