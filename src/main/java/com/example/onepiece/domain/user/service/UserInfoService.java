30e12e5aedb9ad0238544ced6aefe5b2cf26fa0b
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-13 22:32:47 +0900

    ⚡️:: service 추가

--- src/main/java/com/example/onepiece/domain/user/service/UserInfoService.java
+++ src/main/java/com/example/onepiece/domain/user/service/UserInfoService.java
@@ -1,9 +1,31 @@
 package com.example.onepiece.domain.user.service;
 
+import com.example.onepiece.domain.user.domain.User;
+import com.example.onepiece.domain.user.domain.repository.UserRepository;
+import com.example.onepiece.domain.user.exception.UserNotFoundException;
+import com.example.onepiece.domain.user.facade.UserFacade;
+import com.example.onepiece.domain.user.presentation.dto.response.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserInfoService {
+
+    private final UserFacade userFacade;
+    private final UserRepository userRepository;
+
+    public UserInfoResponse userInfo(){
+        User currentUser = userFacade.getCurrentUser();
+        Optional<User>user = userRepository.findByUserId(currentUser.getUserId());
+
+        if (!user.isPresent()){
+            throw UserNotFoundException.EXCEPTION;
+        }
+        else {
+            return new UserInfoResponse(user.get().getNickname());
+        }
+    }
 }
