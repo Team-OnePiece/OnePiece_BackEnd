44b58cd8c18fe6ddb9af6bea83a0c7ea9f3eaa44
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-15 23:57:02 +0900

    ♻️ :: 네이밍 수정

--- src/main/java/com/example/onepiece/domain/user/presentation/dto/request/UserSignUpRequest.java
+++ src/main/java/com/example/onepiece/domain/user/presentation/dto/request/UserSignUpRequest.java
@@ -22,8 +22,7 @@ public class UserSignUpRequest {
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣]{2,8}$", message = "닉네임은 특수문자, 숫자, 영어를 제외한 2~9자 한글 사용하세요.")
     private String nickname;
 
-    @Column(name = "profile_image")
-    private String profile;
+    private String profileImage;
 
     @NotNull
     @Min(1) @Max(3)
