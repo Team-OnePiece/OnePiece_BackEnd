006c4461e8da03b14e7fd105e5a5524fcfef8e6a
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-15 17:15:51 +0900

    ⚡️:: 정규식 추가

--- src/main/java/com/example/onepiece/domain/user/presentation/dto/request/UserModifyRequest.java
+++ src/main/java/com/example/onepiece/domain/user/presentation/dto/request/UserModifyRequest.java
@@ -3,9 +3,14 @@ package com.example.onepiece.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor
 public class UserModifyRequest {
 
+    @NotBlank
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣]{2,8}$", message = "닉네임은 특수문자, 숫자, 영어를 제외한 2~9자 한글 사용하세요.")
     private String nickname;
 }
