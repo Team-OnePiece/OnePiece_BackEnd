f5e147e8e02a2a028f84c5124b176a3294847636
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-15 16:37:22 +0900

    ⚡️:: request추가

---
+++ src/main/java/com/example/onepiece/domain/user/presentation/dto/request/UserModifyRequest.java
@@ -0,0 +1,11 @@
+package com.example.onepiece.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserModifyRequest {
+
+    private String nickname;
+}
