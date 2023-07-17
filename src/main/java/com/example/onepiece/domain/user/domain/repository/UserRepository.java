3379c8759b7b265d8d548d373f8f13da66d31cc8
rudeh2926 <dokyungkim2926@gmail.com>
2023-07-17 22:13:35 +0900

    ♻️ :: 환경변수 설정

--- src/main/java/com/example/onepiece/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/onepiece/domain/user/domain/repository/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserId(String userId);
+    Optional<User> findByAccountId(String accountId);
 
-    boolean existsByUserId(String userId);
+    boolean existsByAccountId(String accountId);
 
     boolean existsByNickname(String nickname);
 }
