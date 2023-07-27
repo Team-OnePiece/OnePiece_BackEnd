b3f27cc2c4bc9441312bf27f4fadd7e98deda9e3
김가은 <kge02160@gmail.com>
2023-07-27 21:17:21 +0900

    📑 :: Tag 엔티티 생성

---
+++ src/main/java/com/example/onepiece/domain/board/domain/Tag.java
@@ -0,0 +1,31 @@
+package com.example.onepiece.domain.board.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @Column(nullable = false, length =30)
+    private String content;
+
+    @Builder
+    public Tag(Board board, String content) {
+        this.board = board;
+        this.content = content;
+    }
+}
