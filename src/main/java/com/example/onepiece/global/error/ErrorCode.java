8886ea5a02b4fe8f7d33d2facd7ba8e3f8717635
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-01 18:59:37 +0900

    ⚡️:: ErrorCode추가

--- src/main/java/com/example/onepiece/global/error/ErrorCode.java
+++ src/main/java/com/example/onepiece/global/error/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "User Not Found"),
     ACCOUNTID_ALREADY_EXISTS(409, "AccountId Already Exists"),
     NICKNAME_ALREADY_EXISTS(409, "Nickname Already Exists"),
+    STUDENTID_ALREADY_EXISTS(409, "StudentId Already Exists"),
     PASSWORD_MISS_MATCH(400, "Password Miss Match"),
 
     JWT_EXPIRED(401, "Jwt Expired"),
