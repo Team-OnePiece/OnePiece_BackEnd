7058345cea20cc264ed2e8d09d631c3d7ccba1c2
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-03 01:09:41 +0900

    ♻️ :: 코드 리펙토링

--- src/main/java/com/example/onepiece/domain/board/presentation/dto/response/BoardIdResponse.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/dto/response/BoardIdResponse.java
@@ -3,7 +3,6 @@ package com.example.onepiece.domain.board.presentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
