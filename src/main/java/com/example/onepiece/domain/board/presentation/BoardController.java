881da7ded140f9d76849a999de3feb45ae1da982
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-04 22:28:14 +0900

    📑 :: 코드 복구

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -1,4 +1,24 @@
 package com.example.onepiece.domain.board.presentation;
 
+import com.example.onepiece.domain.board.presentation.dto.request.BoardRequest;
+import com.example.onepiece.domain.board.presentation.dto.response.BoardIdResponse;
+import com.example.onepiece.domain.board.service.BoardCreateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardCreateService boardCreateService;
+    @PostMapping("/board")
+    public BoardIdResponse writeBoard(@Valid @RequestBody BoardRequest request) {
+        return boardCreateService.writeBoard(request);
+    }
 }
