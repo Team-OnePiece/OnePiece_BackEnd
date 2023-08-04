88a1f3c9d80e48aa4b880d1363e54df59f7c987b
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-04 21:59:44 +0900

    📑 :: 패키지 추가

---
+++ src/main/java/com/example/onepiece/global/entity/BaseTimeEntity.java
@@ -0,0 +1,17 @@
+package com.example.onepiece.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDatetime;
+}
