9b3aa1840901c25e62bb15c9e9b17d4af766660b
김가은 <kge02160@gmail.com>
2023-08-08 15:12:04 +0900

    ⚡️ :: BoardRepository 생성

--- src/main/java/com/example/onepiece/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/onepiece/domain/board/domain/repository/BoardRepository.java
@@ -2,6 +2,10 @@ package com.example.onepiece.domain.board.domain.repository;
 
 import com.example.onepiece.domain.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+import java.util.List;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+
 }
