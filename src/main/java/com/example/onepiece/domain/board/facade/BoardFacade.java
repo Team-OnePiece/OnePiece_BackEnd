000a5ace89dc8812d7eb50c6e4c5729031a95daa
김가은 <kge02160@gmail.com>
2023-08-08 15:20:45 +0900

    ⚡️ :: BoardFacade 생성

---
+++ src/main/java/com/example/onepiece/domain/board/facade/BoardFacade.java
@@ -0,0 +1,20 @@
+package com.example.onepiece.domain.board.facade;
+
+import com.example.onepiece.domain.board.domain.Board;
+import com.example.onepiece.domain.board.domain.repository.BoardRepository;
+import com.example.onepiece.domain.board.exception.BoardNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+
+public class BoardFacade {
+
+    private final BoardRepository boardRepository;
+
+    public Board getBoard(Long boardId) {
+        return boardRepository.findById(boardId)
+                .orElseThrow(() -> BoardNotFoundException.EXCEPTION);
+    }
+}
