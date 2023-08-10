76a88ede44690ed5d7c5a56088560a1502b9f36c
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-10 21:09:06 +0900

    ⚡️ :: tag Delete API 개발

--- src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
+++ src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
@@ -1,14 +1,20 @@
 package com.example.onepiece.domain.tag.presentation;
 
+import com.example.onepiece.domain.tag.presentation.dto.request.TagCreateRequest;
 import com.example.onepiece.domain.tag.service.TagCreateService;
+import com.example.onepiece.domain.tag.service.TagDeleteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @RestController
@@ -17,9 +23,15 @@ import javax.validation.constraints.Size;
 public class TagController {
 
     private final TagCreateService tagCreateService;
+    private final TagDeleteService tagDeleteService;
 
     @PostMapping
-    public void createTag(@RequestParam(value = "tag") @NotNull @Size(min = 1, max = 5) String tag) {
-        tagCreateService.tagCreate(tag);
+    public void createTag(@RequestBody @Valid TagCreateRequest request) {
+        tagCreateService.tagCreate(request);
+    }
+
+    @DeleteMapping("/remove/{tagId}")
+    public void deleteTag(@PathVariable Long tagId) {
+        tagDeleteService.tagDelete(tagId);
     }
 }
