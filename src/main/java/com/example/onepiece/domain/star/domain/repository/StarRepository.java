c42a4ddecaeac85b73bffa546caf475baa4f4e37
Yoohaeun <haspring@dsm.hs.kr>
2023-08-11 00:52:39 +0900

    ⚡️ :: 반응 repository 추가

---
+++ src/main/java/com/example/onepiece/domain/star/domain/repository/StarRepository.java
@@ -0,0 +1,15 @@
+package com.example.onepiece.domain.star.domain.repository;
+
+
+import com.example.onepiece.domain.board.domain.Board;
+import com.example.onepiece.domain.star.domain.Star;
+import com.example.onepiece.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StarRepository extends JpaRepository<Star, Long> {
+
+    Boolean existsByUserAndBoard(User user, Board board);
+    void deleteByUserAndBoard(User user, Board board);
+}
