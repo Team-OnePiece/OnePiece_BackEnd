24fcf7794db2cbe7db2059905082f8b76628ea61
김가은 <kge02160@gmail.com>
2023-08-10 20:04:45 +0900

    ⚡️ :: 공백수정

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -1,11 +1,15 @@
 package com.example.onepiece.domain.board.presentation;
 
-import com.example.onepiece.domain.board.presentation.dto.request.BoardCreateRequest;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardCreateResponse;
 import com.example.onepiece.domain.board.service.BoardCreateService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.validation.Valid;
 
 @RestController
