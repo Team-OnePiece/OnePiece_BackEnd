f78dcd6a45599a24e47d9c7fffc9f0fa474cabcf
김가은 <kge02160@gmail.com>
2023-08-10 15:11:32 +0900

    ⚡️ :: BoardCreateService 수정

--- src/main/java/com/example/onepiece/domain/board/service/BoardCreateService.java
+++ src/main/java/com/example/onepiece/domain/board/service/BoardCreateService.java
@@ -3,7 +3,7 @@ package com.example.onepiece.domain.board.service;
 
 import com.example.onepiece.domain.board.domain.Board;
 import com.example.onepiece.domain.board.domain.repository.BoardRepository;
-import com.example.onepiece.domain.board.presentation.dto.request.BoardRequest;
+import com.example.onepiece.domain.board.presentation.dto.request.BoardCreateRequest;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardIdResponse;
 import com.example.onepiece.domain.user.domain.User;
 import com.example.onepiece.domain.user.facade.UserFacade;
@@ -17,16 +17,14 @@ public class BoardCreateService {
     private final BoardRepository boardRepository;
     private final UserFacade userFacade;
 
-    public BoardIdResponse writeBoard(BoardRequest request) {
+    public BoardIdResponse writeBoard(BoardCreateRequest request) {
         User currentUser = userFacade.getCurrentUser();
         Board board = boardRepository.save(
                 Board.builder()
-                        .user(currentUser)
+                        .writer(currentUser)
                         .place(request.getPlace())
-                        .image(request.getImage())
                         .build()
         );
         return new BoardIdResponse(board.getId());
     }
-
 }
