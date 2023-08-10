2578101ccc5619581fc1e5bcc96973a31dc0827f
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-11 01:42:22 +0900

    🐛 :: 오류수정

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -1,6 +1,5 @@
 package com.example.onepiece.domain.board.presentation;
 
-import com.example.onepiece.domain.board.presentation.dto.request.BoardCreateRequest;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardCreateResponse;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardResponse;
 import com.example.onepiece.domain.board.service.BoardCreateService;
@@ -8,8 +7,8 @@ import com.example.onepiece.domain.board.service.BoardReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,8 +25,8 @@ public class BoardController {
     private final BoardReadService boardReadService;
 
     @PostMapping
-    public BoardCreateResponse writeBoard(@RequestBody @Valid BoardCreateRequest request, @RequestPart(required = false, value = "images") MultipartFile image) {
-        return boardCreateService.writeBoard(request, image);
+    public BoardCreateResponse writeBoard(@RequestParam("place") String place, @RequestPart(required = false, value = "images") MultipartFile image) {
+        return boardCreateService.writeBoard(place, image);
     }
 
     @GetMapping
--- src/main/java/com/example/onepiece/domain/board/service/BoardCreateService.java
+++ src/main/java/com/example/onepiece/domain/board/service/BoardCreateService.java
@@ -3,7 +3,6 @@ package com.example.onepiece.domain.board.service;
 
 import com.example.onepiece.domain.board.domain.Board;
 import com.example.onepiece.domain.board.domain.repository.BoardRepository;
-import com.example.onepiece.domain.board.presentation.dto.request.BoardCreateRequest;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardCreateResponse;
 import com.example.onepiece.domain.user.domain.User;
 import com.example.onepiece.domain.user.facade.UserFacade;
@@ -20,13 +19,13 @@ public class BoardCreateService {
     private final UserFacade userFacade;
     private final S3Facade s3Facade;
 
-    public BoardCreateResponse writeBoard(BoardCreateRequest request, MultipartFile boardImage) {
+    public BoardCreateResponse writeBoard(String place, MultipartFile boardImage) {
 
         User currentUser = userFacade.getCurrentUser();
         Board board = boardRepository.save(
                 Board.builder()
                         .writer(currentUser)
-                        .place(request.getPlace())
+                        .place(place)
                         .build()
         );
 
