844a8fb3ad9331283826207caff69e2eeab6c699
Yoohaeun <haspring@dsm.hs.kr>
2023-08-11 00:52:03 +0900

    ⚡️ :: Star Entity 추가

---
+++ src/main/java/com/example/onepiece/domain/star/domain/Star.java
@@ -0,0 +1,35 @@
+package com.example.onepiece.domain.star.domain;
+
+
+import com.example.onepiece.domain.board.domain.Board;
+import com.example.onepiece.domain.user.domain.User;
+import com.example.onepiece.global.entity.BaseIdEntity;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Star extends BaseIdEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Star(Board board, User user) {
+        this.board = board;
+        this.user = user;
+    }
+
+}
