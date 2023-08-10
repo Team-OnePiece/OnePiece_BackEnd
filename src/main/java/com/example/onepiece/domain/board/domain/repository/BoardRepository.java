c158f85f91bc7c6258bf479f451920688c0a880c
김가은 <kge02160@gmail.com>
2023-08-10 15:02:59 +0900

    ⚡️ :: 공백 수정

--- src/main/java/com/example/onepiece/domain/board/domain/repository/BoardRepository.java
+++ src/main/java/com/example/onepiece/domain/board/domain/repository/BoardRepository.java
@@ -1,10 +1,7 @@
 package com.example.onepiece.domain.board.domain.repository;
 
 import com.example.onepiece.domain.board.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 }
