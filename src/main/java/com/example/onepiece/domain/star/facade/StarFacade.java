e0b1b5b87b13ebc90b871b4e4a89c5963862e1e0
Yoohaeun <haspring@dsm.hs.kr>
2023-08-12 00:04:52 +0900

    ⚰️ :: 기능 삭제

--- src/main/java/com/example/onepiece/domain/star/facade/StarFacade.java
+++ src/main/java/com/example/onepiece/domain/star/facade/StarFacade.java
@@ -2,7 +2,6 @@ package com.example.onepiece.domain.star.facade;
 
 import com.example.onepiece.domain.board.domain.Board;
 import com.example.onepiece.domain.star.domain.repository.StarRepository;
-import com.example.onepiece.domain.star.domain.repository.UnStarRepository;
 import com.example.onepiece.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,13 +11,8 @@ import org.springframework.stereotype.Component;
 public class StarFacade {
 
     private final StarRepository starRepository;
-    private final UnStarRepository unStarRepository;
 
     public boolean checkStar(User user, Board board) {
         return starRepository.existsByUserAndBoard(user, board);
-    }
-
-    public boolean checkUnStar(User user, Board board) {
-        return unStarRepository.existsByUserAndBoard(user, board);
     }
 }
