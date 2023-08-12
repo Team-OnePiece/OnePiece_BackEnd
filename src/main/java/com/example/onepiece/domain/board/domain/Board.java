77786101bd24aafe0ae12a836b3143bc086218fa
김가은 <kge02160@gmail.com>
2023-08-12 19:51:38 +0900

    ♻️ :: 공백 수정, 코드 정렬

--- src/main/java/com/example/onepiece/domain/board/domain/Board.java
+++ src/main/java/com/example/onepiece/domain/board/domain/Board.java
@@ -2,18 +2,18 @@ package com.example.onepiece.domain.board.domain;
 
 import com.example.onepiece.domain.user.domain.User;
 import com.example.onepiece.global.entity.BaseIdEntity;
-import java.time.LocalDate;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
