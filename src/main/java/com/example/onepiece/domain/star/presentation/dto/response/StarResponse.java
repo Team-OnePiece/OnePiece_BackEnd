e216ecfa5fb8e71ee55f24efdf037533b3167523
Yoohaeun <haspring@dsm.hs.kr>
2023-08-12 17:27:27 +0900

    ⚡️ :: 어노테이션 수정

--- src/main/java/com/example/onepiece/domain/star/presentation/dto/response/StarResponse.java
+++ src/main/java/com/example/onepiece/domain/star/presentation/dto/response/StarResponse.java
@@ -1,9 +1,11 @@
 package com.example.onepiece.domain.star.presentation.dto.response;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 @Builder
 public class StarResponse {
