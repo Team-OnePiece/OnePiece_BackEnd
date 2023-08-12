8b642dd3345a4bf9568736e5aa35edcd0f3cbbf8
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-13 03:44:04 +0900

    ⚰️ :: tagList API 삭제

--- src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
+++ src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
@@ -2,14 +2,11 @@ package com.example.onepiece.domain.tag.presentation;
 
 import com.example.onepiece.domain.tag.presentation.dto.request.TagCreateRequest;
 import com.example.onepiece.domain.tag.presentation.dto.response.TagIdResponse;
-import com.example.onepiece.domain.tag.presentation.dto.response.TagMyListResponse;
 import com.example.onepiece.domain.tag.service.TagCreateService;
 import com.example.onepiece.domain.tag.service.TagDeleteService;
-import com.example.onepiece.domain.tag.service.TagMyListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +24,6 @@ public class TagController {
 
     private final TagCreateService tagCreateService;
     private final TagDeleteService tagDeleteService;
-    private final TagMyListService tagMyListService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{boardId}")
@@ -39,10 +35,5 @@ public class TagController {
     @DeleteMapping("/remove/{tagId}")
     public void deleteTag(@PathVariable Long tagId) {
         tagDeleteService.tagDelete(tagId);
-    }
-
-    @GetMapping("/my/list")
-    public List<TagMyListResponse>myListTag() {
-        return tagMyListService.tagMyList();
     }
 }
