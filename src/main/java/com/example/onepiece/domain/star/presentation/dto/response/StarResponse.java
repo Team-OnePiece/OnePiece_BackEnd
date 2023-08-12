c4942ec224577164047e93a34e39562578600152
Yoohaeun <haspring@dsm.hs.kr>
2023-08-12 23:22:54 +0900

    ♻️ :: 공백 수정

--- src/main/java/com/example/onepiece/domain/star/presentation/dto/response/StarResponse.java
+++ src/main/java/com/example/onepiece/domain/star/presentation/dto/response/StarResponse.java
@@ -1,6 +1,7 @@
 package com.example.onepiece.domain.star.presentation.dto.response;
 
-
+import com.example.onepiece.domain.board.domain.Board;
+import com.example.onepiece.domain.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
