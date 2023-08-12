8187b548e61d4a9f48e26666ae598ed7c9e8c8ea
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-12 22:47:28 +0900

    ⚡️ :: BoardCreate API 분리

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -7,16 +7,10 @@ import com.example.onepiece.domain.board.service.BoardImageUploadService;
 import com.example.onepiece.domain.board.service.QueryBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
@@ -30,7 +24,7 @@ public class BoardController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public BoardCreateResponse writeBoard(@RequestParam("place") String place) {
+    public BoardCreateResponse writeBoard(@RequestBody @NotNull String place) {
         return boardCreateService.writeBoard(place);
     }
 
