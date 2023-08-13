0b15210638adf29aeed96046a787d3af1d9c4fb0
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-13 22:54:57 +0900

    ♻️ :: 메소드명 변경

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -9,10 +9,8 @@ import com.example.onepiece.domain.board.service.QueryBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.example.onepiece.domain.board.service.QueryBoardService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,11 +44,12 @@ public class BoardController {
     }
 
     @PatchMapping("/{boardId}")
-    public void modifyBoard(@PathVariable(value = "boardId") Long boardId, @RequestParam(value = "place") String place, @RequestPart(required = false, value = "image") MultipartFile image) {
+    public void boardDelete(@PathVariable(value = "boardId") Long boardId, @RequestParam(value = "place") String place, @RequestPart(required = false, value = "image") MultipartFile image) {
         boardModifyService.modifyBoard(boardId, place, image);
-  
+    }
+
     @DeleteMapping("/{boardId}")
-    public void deleteBoard(@PathVariable Long boardId) {
+    public void boardDelete(@PathVariable(value = "boardId") Long boardId) {
         deleteBoardService.deleteBoard(boardId);
     }
 }
