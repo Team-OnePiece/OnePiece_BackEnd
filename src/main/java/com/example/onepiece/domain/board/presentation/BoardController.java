9755a60910572ac5f095f1cf666dd73b4398a337
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-13 14:12:26 +0900

    ⚰️ :: board ImageUpload API 삭제

--- src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/onepiece/domain/board/presentation/BoardController.java
@@ -3,14 +3,12 @@ package com.example.onepiece.domain.board.presentation;
 import com.example.onepiece.domain.board.presentation.dto.response.BoardCreateResponse;
 import com.example.onepiece.domain.board.presentation.dto.response.QueryBoardResponse;
 import com.example.onepiece.domain.board.service.BoardCreateService;
-import com.example.onepiece.domain.board.service.BoardImageUploadService;
 import com.example.onepiece.domain.board.service.QueryBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
@@ -20,18 +18,11 @@ public class BoardController {
 
     private final BoardCreateService boardCreateService;
     private final QueryBoardService queryBoardService;
-    private final BoardImageUploadService boardImageUploadService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public BoardCreateResponse writeBoard(@RequestParam(value = "place") String place, @RequestPart(required = false, value = "image") MultipartFile image) {
         return boardCreateService.writeBoard(place, image);
-    }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/image/upload/{boardId}")
-    public void boardImageUpload(@PathVariable(value = "boardId") Long boardId, @RequestPart(required = false, value = "image") MultipartFile image) {
-        boardImageUploadService.uploadBoardImage(image, boardId);
     }
 
     @GetMapping("/all")
