86d602befd476f7a3dccad5ab6c8f6c570cb986d
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-13 23:39:28 +0900

    ♻️ :: 패키지 구조 수정

--- src/main/java/com/example/onepiece/domain/star/presentation/StarController.java
+++ src/main/java/com/example/onepiece/domain/star/presentation/StarController.java
@@ -2,7 +2,8 @@ package com.example.onepiece.domain.star.presentation;
 
 
 import com.example.onepiece.domain.star.presentation.dto.response.StarResponse;
-import com.example.onepiece.domain.star.service.StarService;
+import com.example.onepiece.domain.star.service.CreateStarService;
+import com.example.onepiece.domain.star.service.RemoveStarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,17 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/star")
 @RestController
 public class StarController {
-    private final StarService starService;
+
+    private final CreateStarService createStarService;
+    private final RemoveStarService removeStarService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{boardId}/star")
     public StarResponse star(@PathVariable("boardId") Long boardId){
-        return starService.createStar(boardId);
+        return createStarService.createStar(boardId);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/{boardId}/delete-star")
     public StarResponse deleteStar(@PathVariable("boardId") Long boardId){
-        return starService.deleteStar(boardId);
+        return removeStarService.deleteStar(boardId);
     }
 }
--- src/main/java/com/example/onepiece/domain/star/service/RemoveStarService.java
+++ src/main/java/com/example/onepiece/domain/star/service/RemoveStarService.java
@@ -1,41 +1,26 @@
 package com.example.onepiece.domain.star.service;
 
-
 import com.example.onepiece.domain.board.domain.Board;
 import com.example.onepiece.domain.board.facade.BoardFacade;
-import com.example.onepiece.domain.star.domain.Star;
 import com.example.onepiece.domain.star.domain.repository.StarRepository;
 import com.example.onepiece.domain.star.exception.RemoveStarExistException;
-import com.example.onepiece.domain.star.exception.StarExistException;
 import com.example.onepiece.domain.star.facade.StarFacade;
 import com.example.onepiece.domain.star.presentation.dto.response.StarResponse;
 import com.example.onepiece.domain.user.domain.User;
 import com.example.onepiece.domain.user.facade.UserFacade;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Service
-public class StarService {
+import javax.transaction.Transactional;
 
-    private final StarRepository starRepository;
+@Service
+@RequiredArgsConstructor
+public class RemoveStarService {
+
     private final UserFacade userFacade;
     private final BoardFacade boardFacade;
     private final StarFacade starFacade;
-
-    @Transactional
-    public StarResponse createStar(Long boardId) {
-        User user = userFacade.getCurrentUser();
-        Board board = boardFacade.getBoard(boardId);
-
-        if (starFacade.hasUserGivenStarToBoard(user, board)) {
-            throw StarExistException.EXCEPTION;
-        }
-
-        board.addStarCount();
-        return addStar(user, board);
-    }
+    private final StarRepository starRepository;
 
     @Transactional
     public StarResponse deleteStar(Long boardId) {
@@ -50,20 +35,10 @@ public class StarService {
         return removeStar(user, board);
     }
 
-    private StarResponse addStar(User user, Board board) {
-        starRepository.save(Star.builder()
-            .user(user)
-            .board(board)
-            .build());
-
-        return new StarResponse(board.getStarCounts(),
-            starFacade.hasUserGivenStarToBoard(user, board));
-    }
-
     private StarResponse removeStar(User user, Board board) {
         starRepository.deleteByUserAndBoard(user, board);
 
         return new StarResponse(board.getStarCounts(),
-            starFacade.hasUserGivenStarToBoard(user, board));
+                starFacade.hasUserGivenStarToBoard(user, board));
     }
 }
