0e3241e01d0c62f65ae9fa52c1d42ce1311229c0
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-14 16:41:37 +0900

    ♻️ :: annotation import 와일드카드로 변경

--- src/main/java/com/example/onepiece/domain/group/presentation/GroupController.java
+++ src/main/java/com/example/onepiece/domain/group/presentation/GroupController.java
@@ -3,8 +3,7 @@ package com.example.onepiece.domain.group.presentation;
 import com.example.onepiece.domain.group.presentation.dto.response.QueryGroupResponse;
 import com.example.onepiece.domain.group.service.QueryGroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
+++ src/main/java/com/example/onepiece/domain/tag/presentation/TagController.java
@@ -6,13 +6,7 @@ import com.example.onepiece.domain.tag.service.TagCreateService;
 import com.example.onepiece.domain.tag.service.TagDeleteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
