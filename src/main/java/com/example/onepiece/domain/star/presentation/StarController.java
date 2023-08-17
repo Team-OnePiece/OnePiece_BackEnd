79c55c27e2a0b1911fa21906cbbd5c84a2128ead
rudeh2926 <dokyungkim2926@gmail.com>
2023-08-17 22:28:05 +0900

    ⚡️ :: StarDelete Url수정

--- src/main/java/com/example/onepiece/domain/star/presentation/StarController.java
+++ src/main/java/com/example/onepiece/domain/star/presentation/StarController.java
@@ -23,7 +23,7 @@ public class StarController {
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PostMapping("/delete/{feedId}")
+    @DeleteMapping("/{feedId}")
     public StarResponse deleteStar(@PathVariable("feedId") Long feedId) {
         return removeStarService.deleteStar(feedId);
     }
